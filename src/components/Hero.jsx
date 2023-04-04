7c7831fc647f079f7974bdd9d1df8537641bca54
helena <helena.gilja@gmail.com>
2023-04-04 23:03:27 +0100

    hero doooneeee

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -10,16 +10,16 @@ const Hero = () => {
         className={`${styles.paddingX} absolute inset-0 top-[120px] max-w-7xl mx-auto flex flex-row items-start gap-5`}
       >
         <div className="flex flex-col justify-center items-center mt-5">
-          <div className="w-5 h-5 rounded-full bg-[#915eff]" />
-          <div className="w-1 sm:h-80 h-40 violet-gradient" />
+          <div className="w-5 h-5 rounded-full bg-[#fff]" />
+          <div className="w-1 sm:h-80 h-40 white-gradient" />
         </div>
 
         <div>
           <h1 className={`${styles.heroHeadText}`}>
-            Hi, I'm <span className="text-[#915eff]">Helena</span>{" "}
+            Hi, I'm <span className="text-[#db0606]">Helena</span>{" "}
           </h1>
           <p className={`${styles.heroSubText} mt-2 text-white-100`}>
-            I develop user interfaces <br className="sm:block hidden" /> and web
+            I develop user-friendly interfaces <br className="sm:block hidden" /> and web
             applications.
           </p>
         </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,7 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        primary: "#050816",
+        primary: "#000000",
         secondary: "#aaa6c3",
         tertiary: "#151030",
         "black-100": "#100d25",
@@ -19,7 +19,7 @@ module.exports = {
         xs: "450px",
       },
       backgroundImage: {
-        "hero-pattern": "url('/src/assets/herobg.png')",
+        "hero-pattern": "url('/src/assets/bgblack.png')",
       },
     },
   },
