df6f8101d462aca5649e735633ad96311c4e9b3a
helena <helena.gilja@gmail.com>
2023-04-04 23:33:57 +0100

    hero color change

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -16,7 +16,7 @@ const Hero = () => {
 
         <div>
           <h1 className={`${styles.heroHeadText}`}>
-            Hi, I'm <span className="text-[#db0606]">Helena</span>{" "}
+            Hi, I'm <span className="text-[#e07171]">Helena</span>{" "}
           </h1>
           <p className={`${styles.heroSubText} mt-2 text-white-100`}>
             I develop user-friendly interfaces <br className="sm:block hidden" /> and web
--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -1,8 +1,8 @@
 import React, { Suspense, useEffect, useState } from "react";
 import { Canvas } from "@react-three/fiber";
 import { OrbitControls, Preload, useGLTF } from "@react-three/drei";
-import { extend } from '@react-three/fiber'
-extend({ OrbitControls })
+// import { extend } from '@react-three/fiber'
+// extend({ OrbitControls })
 
 
 import CanvasLoader from "../Loader";
@@ -12,19 +12,19 @@ const Computers = React.memo(({ isMobile }) => {
 
   return (
     <group>
-    <hemisphereLight intensity={0.15} groundColor="black" />
+    <hemisphereLight intensity={0.65} groundColor="black" />
     <spotLight
       position={[-20, 50, 10]}
-      angle={0.12}
+      angle={0.19}
       penumbra={1}
       intensity={1}
       castShadow
       shadow-mapSize={1024}/>
-    <pointLight intensity={1} />
+    <pointLight intensity={1.5} />
     <group // create a new group to hold the custom object
-      scale={isMobile ? 0.7 : 0.75}
-      position={isMobile ? [1.5, -3, -2.2] : [0.7, -3.25, -1.5]}
-      rotation={[0, -Math.PI / 18, 0]}>
+      scale={isMobile ? 0.72 : 0.82}
+      position={isMobile ? [-0.2, -3, -2.2] : [0.7, -3.25, -1.5]} // array of x y and z axes
+      rotation={[0.1, -Math.PI / 18, 0]}>
       <primitive object={computer.scene} />
     </group>
   </group>
